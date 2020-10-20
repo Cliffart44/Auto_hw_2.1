a729e331ace6167ab2fbb668a50eca28e9659df6
Cliffart <Cliffart44@gmail.com>
2020-10-20 15:25:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### [Решение](https://github.com/Cliffart44/Auto_hw_2.1) => [домашнего задания](https://github.com/netology-code/aqa-homeworks/tree/aqa4/web#%D0%B7%D0%B0%D0%B4%D0%B0%D1%87%D0%B0-1---%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7-%D0%BA%D0%B0%D1%80%D1%82%D1%8B)
+
+---
+[![Build status](https://ci.appveyor.com/api/projects/status/i36n26xy2m3slm9v/branch/main?svg=true)](https://ci.appveyor.com/project/Cliffart44/auto-hw-2-1/branch/main)
---
+++ src/test/java/PageUiTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+class PageUiTest {
+    WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--headless", "--disable-gpu"));
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "artifacts/chromedriver86");
+    }
+
+    @AfterEach
+    void down() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Альберт Эйнштейн");
+        elements.get(1).sendKeys("+14318791955");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.tagName("p")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+}
