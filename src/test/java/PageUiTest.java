e74443c16992b170b32569c1a52ae37d019261c6
Cliffart <Cliffart44@gmail.com>
2020-10-23 17:56:26 +0300

    Further error messages checking

--- src/test/java/PageUiTest.java
+++ src/test/java/PageUiTest.java
@@ -3,13 +3,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
 
 class PageUiTest {
     WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--headless", "--disable-gpu"));
@@ -26,14 +23,46 @@ class PageUiTest {
     }
 
     @Test
-    public void shouldSubmitRequest(){
+    public void shouldSubmitAlertByEmptyName(){
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("1Альберт Эйнштейн");
-        elements.get(1).sendKeys("+14318791955");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__content")).click();
-        String text = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/div[1]/span/span/span[3]")).getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+14318791955");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Фамилия и имя']/following-sibling::span[contains(@class, 'input__sub')]")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    @Test
+    public void shouldSubmitAlertByWrongName(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("+14318791955");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+14318791955");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Фамилия и имя']/following-sibling::span[contains(@class, 'input__sub')]")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+    }
+
+    @Test
+    public void shouldSubmitAlertByEmptyPhone(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Альберт Эйнштейн");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Мобильный телефон']/following-sibling::span[contains(@class, 'input__sub')]")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text);
+    }
+
+    @Test
+    public void shouldSubmitAlertByWrongPhone(){
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Альберт Эйнштейн");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Альберт Эйнштейн");
+        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(By.cssSelector(".form-field .button__content")).click();
+        String text = driver.findElement(By.xpath("//span[text()='Мобильный телефон']/following-sibling::span[contains(@class, 'input__sub')]")).getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
     }
 }
